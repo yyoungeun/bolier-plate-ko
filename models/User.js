9c19353e7ba64f6d5b70fc151a65e7cc4e8a86bf
yyoungeun <1150amy@naver.com>
2020-04-17 13:56:35 +0900

    0417_bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose'); //mongoose module
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -31,6 +33,22 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+    //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+})
 const User = mongoose.model('User', userSchema) 
 
 module.exports = {User}
