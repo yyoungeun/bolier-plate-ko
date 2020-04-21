e7302bd24ac5b72316d15984121caac0b489b45f
yyoungeun <1150amy@naver.com>
2020-04-21 17:08:14 +0900

    0421_login

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!~~안녕'))  //root directory에 오면 hello world 출력
 
-app.post('/api/users/register', (req,res) => {
+app.post('/register', (req,res) => {
     //회원가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
 
     const user = new User(req.body)
 
-    user.save((err, userInfo) =>{
+    user.save((err, user) =>{
         if(err) return res.json({success: false, err})
         return res.status(200).json({
             success: true
@@ -37,7 +37,7 @@ app.post('/api/users/register', (req,res) => {
     })
 })
 
-app.post('/api/users/login', (req,res) => {
+app.post('/login', (req,res) => {
 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
@@ -45,13 +45,16 @@ app.post('/api/users/login', (req,res) => {
             return res.json({
                 loginSuccess: false,
                 message: "제공된 이메일에 해당하는 유저가 없습니다."
-            });
+            })
         }
         //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
         user.comparePassword(req.body.password, (err, isMatch) => {
 
             if(!isMatch)
-            return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+            return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 틀렸습니다."
+            })
 
             //비밀번호까지 맞다면 토큰을 생성하기.
             user.generateToken((err, user) => {
